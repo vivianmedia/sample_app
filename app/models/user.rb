1530e3d8927d3f1b528412f77c0ab80b3fff96fa
vivianmedia <viviankhoung@gmail.com>
2011-11-04 16:51:38 -0700

    User model valiadations

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,13 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessible :username, :email
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :username,  :presence => true,
+                    :length   => { :maximum => 50 }
+  validates :email, :presence => true,
+                    :format   => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
 end
