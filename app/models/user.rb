3027dfe4c075ea9cd57505879d363b09189758ce
vivianmedia <viviankhoung@gmail.com>
2011-11-15 16:09:54 -0800

    added a stub user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,27 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id            :integer         not null, primary key
-#  created_at    :datetime
-#  updated_at    :datetime
-#  username      :string(255)
-#  email         :string(255)
-#  password_hash :string(255)
-#  pssword_salt  :string(255)
-#
-
 class User < ActiveRecord::Base
-  attr_accessible :username, :email
+  attr_accessor   :password
+  attr_accessible :username, :email, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
-  validates :username,  :presence => true,
+  validates :username, :presence => true,
                     :length   => { :maximum => 50 }
   validates :email, :presence => true,
                     :format   => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  username   :string(255)
+#  email      :string(255)
+#
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe User do
 
   before(:each) do
-    @attr = { :username => "Example User", :email => "user@example.com" }
+    @attr = { :username => "Example User", :email => "user@example.com", :password => "foorbar", :password_confirmation => "foorbar"}
   end
 
   it "should create a new instance given valid attributes" do
@@ -54,20 +54,57 @@ describe User do
        user_with_duplicate_email = User.new(@attr)
        user_with_duplicate_email.should_not be_valid
      end
-  
+     
+     describe "passwords" do
+       before(:each) do
+          @user = User.new(@attr)
+        end
+       
+       it "should have a password attribute" do
+         @user.should respond_to(:password)
+       end
+       
+       it "should have a password confirmation attribute" do
+         @user.should respond_to(:password_confirmation)
+       end
+     end
+     
+     describe "password validations" do
+
+        it "should require a password" do
+          User.new(@attr.merge(:password => "", :password_confirmation => "")).
+            should_not be_valid
+        end
+
+        it "should require a matching password confirmation" do
+          User.new(@attr.merge(:password_confirmation => "invalid")).
+            should_not be_valid
+        end
+
+        it "should reject short passwords" do
+          short = "a" * 5
+          hash = @attr.merge(:password => short, :password_confirmation => short)
+          User.new(hash).should_not be_valid
+        end
+
+        it "should reject long passwords" do
+          long = "a" * 41
+          hash = @attr.merge(:password => long, :password_confirmation => long)
+          User.new(hash).should_not be_valid
+        end
+      end
 end
+
 
 
 # == Schema Information
 #
 # Table name: users
 #
-#  id            :integer         not null, primary key
-#  created_at    :datetime
-#  updated_at    :datetime
-#  username      :string(255)
-#  email         :string(255)
-#  password_hash :string(255)
-#  pssword_salt  :string(255)
+#  id         :integer         not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  username   :string(255)
+#  email      :string(255)
 #
 
